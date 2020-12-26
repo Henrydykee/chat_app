aab2c780d805612bcdd2ee387c66f7420d357dcf
Ugochukwu <dykeehenry@gmail.com>
2020-12-26 22:52:09 +0100

    conecting to db

--- controllers/users.js
+++ controllers/users.js
@@ -3,9 +3,15 @@ module.exports = function(){
     return{
         SetRouting: function(router){
             router.get('/',this.indexPage)
+            router.get('/signup', this.getSignUp)
         },
+
         indexPage: function(req,res){
             return res.render('index', {test: 'this is a test'})
+        },
+
+        getSignUp: function(req,res){
+            return res.render('signup')
         }
     }
 }
--- sever.js
+++ sever.js
@@ -5,8 +5,26 @@ const http = require('http');
 const container = require('./container');
 const users = require('./controllers/users');
 const color = require('colors')
+const cookieParser = require('cookie-parser');
+const validator = require ('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport')
+
+
+
 
 container.resolve(function (users) {
+
+    mongoose.Promise = global.Promise;
+    mongoose.connect(`${process.env.MONGO_URI}`,{
+       useMongoClient: true,
+       useNewUrlParser: true
+    });
+    //console.log(`MongoDb Connected:${connect.connection.host}`.green.underline);
+
     const app = SetupExpress();
 
     function SetupExpress() {
@@ -26,7 +44,21 @@ container.resolve(function (users) {
         app.use(express.static('public'));
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(cookieParser());
+        //app.use(validator());
+        app.use(session({
+            secret: 'asdkljjhhhjkkknk',
+            resave: true,
+            saveInitialized: true,
+            stroe: new MongoStore({
+                mongooseConnection: mongoose.connection
+            })
+        }))
+        app.use(flash())
+
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
 });
 
